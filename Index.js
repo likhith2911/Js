a6fc03fa5bcd23319dd67982387b455e6635d924
likhith2911 <likhithvasudev2911@gmail.com>
2023-12-22 11:50:58 +0530

    Java Script file

---
+++ Index.js
@@ -0,0 +1,18 @@
+// console.log("hiiiiiiiii")
+// console.log(10-10+"hi")
+// document.write("hiiiiiiiiiii")
+// document.writeln("helloooooo")
+// document.write("hiiiiiiiiiii")
+// //var name=prompt("hi!!!!!!!!! what is your name")
+// //alert("scnsancksnaknKN")
+// console.log(1-'1')
+// console.log(1/2)
+// console.log(1/"hi")
+// console.log(1+10-10+12*2+"1"-2)
+// console.log(1/0)
+//console.log(num);
+// var num=12;
+// let num=19;
+// console.log(num)
+console.log(num);
+let num=10;
